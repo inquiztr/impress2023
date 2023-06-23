03e14b4db93e040fe7a24e834b98c6982d4d4e52
Robin Bank <robin@northerncoder.ca>
2023-06-22 23:58:06 -0400

    gallery

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -2,13 +2,21 @@ import styles from './gallery.module.scss'
 import { fontMontserrat, fontOpen } from '../utils/fonts'
 import Image from 'next/image'
 import grey from '../../../public/grey.png'
+import aa from './june02.jpg'
+import bb from './june03.jpg'
+import cc from './june04.jpg'
+import dd from './june05.jpg'
+import ee from './june06.jpg'
+import ff from './june07.jpg'
+import gg from './june08.jpg'
+import hh from './june09.jpg'
 function Gallery() {
     return (
         <>
             <section id="gallery" className={styles.galleryHeadline}>
                 <div className={styles.galleryWrap}>
                     <div className={styles.col1}>
-                        <h4 className={fontMontserrat.className}>News</h4>
+                        <h4 className={fontMontserrat.className}>Gallery</h4>
                     </div>
                     {/* <Image
                         alt="background"
@@ -20,24 +28,121 @@ function Gallery() {
                         style={{
                             objectFit: 'cover',
                         }}
-                    />   */}
-                    <div className={styles.col2}>
-                        <h5>25th Anniversary Celebration</h5>
-                        <p className={fontOpen.className}>
-                            Thurs, June 15th,
-                            <br />
-                            6pm - 10pm
-                        </p>
-                    </div>
-                    <div className={styles.col3}>
-                        <a href="">
-                            {/* <Image
-                                src={headlineImage}
-                                alt=""
-                                width="150"
-                                height="140"
-                            /> */}
-                        </a>{' '}
+                    /> */}
+
+                    <div className={styles.grid}>
+                        <div className={styles.image}>
+                            <Image
+                                alt="store products"
+                                src={aa}
+                                placeholder="blur"
+                                quality={100}
+                                width="250"
+                                height="250"
+                                style={{
+                                    objectFit: 'cover',
+                                }}
+                            />
+                            <div className={styles.overlay}></div>
+                        </div>
+                        <div className={styles.image}>
+                            <Image
+                                alt="store products"
+                                src={bb}
+                                placeholder="blur"
+                                quality={100}
+                                width="250"
+                                height="250"
+                                style={{
+                                    objectFit: 'cover',
+                                }}
+                            />
+                            <div className={styles.overlay}></div>
+                        </div>
+                        <div className={styles.image}>
+                            <Image
+                                alt="store products"
+                                src={cc}
+                                placeholder="blur"
+                                quality={100}
+                                width="250"
+                                height="250"
+                                style={{
+                                    objectFit: 'cover',
+                                }}
+                            />
+                            <div className={styles.overlay}></div>
+                        </div>
+                        <div className={styles.image}>
+                            <Image
+                                alt="store products"
+                                src={dd}
+                                placeholder="blur"
+                                quality={100}
+                                width="250"
+                                height="250"
+                                style={{
+                                    objectFit: 'cover',
+                                }}
+                            />
+                            <div className={styles.overlay}></div>
+                        </div>
+                        <div className={styles.image}>
+                            <Image
+                                alt="store products"
+                                src={ee}
+                                placeholder="blur"
+                                quality={100}
+                                width="250"
+                                height="250"
+                                style={{
+                                    objectFit: 'cover',
+                                }}
+                            />
+                            <div className={styles.overlay}></div>
+                        </div>
+                        <div className={styles.image}>
+                            <Image
+                                alt="store products"
+                                src={ff}
+                                placeholder="blur"
+                                quality={100}
+                                width="250"
+                                height="250"
+                                style={{
+                                    objectFit: 'cover',
+                                }}
+                            />
+                            <div className={styles.overlay}></div>
+                        </div>
+                        <div className={styles.image}>
+                            <Image
+                                alt="store products"
+                                src={gg}
+                                placeholder="blur"
+                                quality={100}
+                                width="250"
+                                height="250"
+                                style={{
+                                    objectFit: 'cover',
+                                }}
+                            />
+                            <div className={styles.overlay}></div>
+                        </div>
+                        <div className={styles.image}>
+                            <Image
+                                alt="store products"
+                                src={hh}
+                                placeholder="blur"
+                                quality={100}
+                                width="250"
+                                height="250"
+                                style={{
+                                    objectFit: 'cover',
+                                }}
+                            />
+                            <div className={styles.overlay}></div>
+                        </div>
                     </div>
                 </div>
             </section>
