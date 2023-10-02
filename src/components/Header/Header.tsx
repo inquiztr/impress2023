cbbe9307d18b8497a821904f9cc4bd7b5a1513d7
Robin Bank <robin@northerncoder.ca>
2023-10-02 17:32:34 -0400

    class added to logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -42,7 +42,6 @@ export default function Header() {
                             height="53"
                             priority
                             placeholder="empty"
-                            className={styles.logoImage}
                         />
                     </Link>
                 </div>
